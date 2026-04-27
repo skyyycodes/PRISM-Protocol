cd537c60782887965cb41600113565e091367a41
Akash Chakraborty <akashchakraborty694@gmail.com>
2026-04-27 23:41:27 +0530

    fix: update DOCS_URL to use the correct production link

--- lib/site-links.ts
+++ lib/site-links.ts
@@ -1,2 +1,3 @@
-export const DOCS_URL = process.env.NEXT_PUBLIC_DOCS_URL || "http://127.0.0.1:3001/";
+export const DOCS_URL =
+  process.env.NEXT_PUBLIC_DOCS_URL || "https://docs.prismprotocol.dev/";
 export const TWITTER_URL = "https://x.com/prismsolana";
